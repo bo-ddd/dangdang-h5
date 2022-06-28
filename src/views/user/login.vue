10c62d88eaf594d6619832fbf01ef216728a0473
lwb <1375229126@qq.com>
2022-06-28 16:10:11 +0800

    修改了公共样式

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,5 +1,13 @@
 <template>
-    <div>
+    <div class="wrap">
         thsi is a login page 
     </div>
 </template>
+
+<style>
+.wrap{
+    background: #567df6;
+    border-radius: 2rem;
+    height: 100vh;
+}
+</style>
